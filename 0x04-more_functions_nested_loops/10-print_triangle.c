1a3ddf035ec9fe2c4f381fad607da37711fa4783
DenopJ <denop87@yahoo.com>
2023-02-23 16:35:52 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,17 +9,24 @@
 
 void print_triangle(int size)
 {
-	int x, y;
+	int x, y, z;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-
-	for (x = 1; x <= size; x++)
-		for (y = 1; y <= 1; y++)
+	else
+	{
+		for (x = 1; x <= size; x++)
 		{
-			_putchar('#');
+			for (z = size - x; z >= 1; z--)
+			{
+				_putchar(' ');
+			}
+			for (y = 1; y <= x; y++)
+			{
+				_putchar('#');
+			}
 		}
-		_putchar('\n');
+	_putchar('\n');
 }
