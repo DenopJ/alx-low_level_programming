df87d70866dcc9cac0fc84f39d12513b63cb55c2
DenopJ <denop87@yahoo.com>
2023-02-27 19:06:58 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,33 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
+	int a = 0;
+	unsigned int x = 0;
+	int y = 1;
+	int z = 0;
 
-	while (*s == ' ')
-		s++;
-
-	if (*s == '+' || *s == '-')
+	while (s[a])
 	{
-		if (*s == '-')
-			sign = -1;
-		s++;
+		if (s[a] == 45)
+		{
+			y *= -1;
+		}
+
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			z = 1;
+			x = (x * 10) + (s[a] - '0');
+			a++;
+		}
+
+		if (z == 1)
+		{
+			break;
+		}
+
+		a++;
 	}
 
-	while (*s >= '0' && *s <= '9')
-	{
-		result = result * 10 + (*s - '0');
-		s++;
-	}
-
-	return (result * sign);
+	x *= y;
+	return (x);
 }
