5da1bcab2e45b45352b9319a111cc898781a0886
DenopJ <denop87@yahoo.com>
2023-03-02 02:48:51 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,22 +14,17 @@ char *_strncat(char *dest, char *src, int n)
 	int x,
 	int y;
 
-	x = 0;
-
-	while (dest[x] != '\0')
+	for (x = 0; dest[x] != '\0'; x++)
 	{
-		x++;
+		continue;
 	}
 
-	y = 0;
-
-	while (y < n && src[y] != '\0')
+	for (y = 0; src[y] != '\0' && y < n; y++)
 	{
-		dest[x] = src[y];
-		x++;
-		y++;
+		dest[x + y] = src[y];
 	}
-	dest[x] = '\0';
+	dest[x + y] = '\0';
+
 	return (dest);
 }
 
