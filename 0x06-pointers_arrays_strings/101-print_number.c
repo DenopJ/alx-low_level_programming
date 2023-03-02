819ab1e281dc21aee74096243e79b219b5d61f0d
DenopJ <denop87@yahoo.com>
2023-03-02 10:22:35 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,18 @@
 
 void print_number(int n)
 {
+	unsigned int a;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n *= -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	n = a
 
-	_putchar((n % 10 + '0');
+	if (a / 10)
+		print_number(a / 10);
+
+	_putchar((a % 10) + '0');
 }
