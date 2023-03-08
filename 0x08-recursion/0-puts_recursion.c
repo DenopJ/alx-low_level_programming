eaddc98c5958735c2119f2776bafeeeac67f60ed
DenopJ <denop87@yahoo.com>
2023-03-08 14:56:36 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string
+ * @s: Parameter
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
