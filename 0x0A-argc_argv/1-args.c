ba200691bbcf83bbb500854468ee018d30dad2ce
DenopJ <denop87@yahoo.com>
2023-03-10 07:18:35 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints the number of arguments passed into it.
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
