adf070fac98a4517f03f863dd7df3c27599446ee
DenopJ <denop87@yahoo.com>
2023-03-14 16:39:10 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	ptr = (int *) malloc(height * sizeof(int *));
+	ptr = (int **) malloc(height * sizeof(int *));
 
 	if (ptr == NULL)
 		return (NULL);
